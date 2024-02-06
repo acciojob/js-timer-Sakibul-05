34934e381e9a77d1f82353160d3f7008b457b87f
Sk Sakibul Islam <110473516+Sakibul-05@users.noreply.github.com>
2024-02-06 23:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // write js code here if required
+
+const timerElement = document.querySelector("#timer");
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth()+1;
+let day = date.getDate();
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+timerElement.innerHTML = `${day}/${month}/${year}, ${hour}:${minute}:${second}`;
